93083a66763f67a6774906c4327c6c130943fedf
Ben <shatterwaltz@gmail.com>
2024-06-11 22:49:18 -0400

    comments in conversation resource

--- DataTypes/conversation.gd
+++ DataTypes/conversation.gd
@@ -1,6 +1,13 @@
 extends Resource
 class_name Conversation
 
+#Denotes if this conversation is text or a list of options
 @export var is_choice: bool
+
+#If is a choice, this will be used as list of options
 @export var dialogue: Array[String]
+
+#If not a choice, [0] is automatically run
+#If is a choice, dialogue[n] will trigger next_conversation[n]
+#Any null value will end the conversation
 @export var next_conversation: Array[Conversation]
